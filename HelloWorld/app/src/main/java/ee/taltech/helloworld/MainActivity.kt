6cfa066f71e2bd4f51b7f0ef8e526db2b4b09771
Jevgeni Fenko <jefenk@taltech.ee>
2020-09-30 03:49:46 +0300

    connect 4 design

---
+++ HelloWorld/app/src/main/java/ee/taltech/helloworld/MainActivity.kt
@@ -0,0 +1,19 @@
+package ee.taltech.helloworld
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    @SuppressLint("SetTextI18n")
+    fun buttonOKonClick(view: View) {
+        textView.text = "Hello, " + plainTextName.editableText + "!"
+    }
+}
