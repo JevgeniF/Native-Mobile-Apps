9fa5f87065f23b7984ddbaa123bc17157d42ae5a
Jevgeni Fenko <jefenk@taltech.ee>
2020-10-06 08:37:59 +0300

    Game Logic missing

--- Connect4/app/src/main/java/ee/taltech/connect4/MainActivity.kt
+++ Connect4/app/src/main/java/ee/taltech/connect4/MainActivity.kt
@@ -1,11 +1,24 @@
 package ee.taltech.connect4
 
+import android.icu.number.IntegerWidth
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.core.content.ContextCompat
+
 
 class MainActivity : AppCompatActivity() {
+    var nextMoveByPOne = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun gameButtonOnClick(view: View) {
+        if ((view as Button).text == "") {
+            val tag = (Integer.parseInt((view.tag).toString()))
+            println(view.id);
+        }
     }
 }
